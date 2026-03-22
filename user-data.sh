14d6c86c22f393725aa467d332c64129a3c26f1f
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2026-03-22 13:50:14 -0700

    final script?

--- user-data.sh
+++ user-data.sh
@@ -2,10 +2,11 @@
 
 # grab and install packages
 dnf update -y
+dnf install -y dnf-plugins-core
 dnf config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-dnf install -y java-21-amazon-corretto-devel dnf-plugins-core fontconfig terraform jenkins
+dnf install -y java-21-amazon-corretto-devel fontconfig terraform jenkins
 
 # make new jenkins temp dir
 mkdir -p /var/lib/jenkins/tmp
@@ -20,17 +21,16 @@ Environment="JAVA_OPTS=-Djava.io.tmpdir=/var/lib/jenkins/tmp"
 EOF
 
 # install plugins
-cd /home/ec2-user
-
-curl -fLs -o jenkins-plugin-manager-2.14.0.jar \
+curl -fLs -o /tmp/jenkins-plugin-manager.jar \
   https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.14.0/jenkins-plugin-manager-2.14.0.jar
 
-curl -Os https://raw.githubusercontent.com/aaron-dm-mcdonald/new-jenkins-s3-test/refs/heads/main/plugins.yaml
+curl -fLs -o /tmp/plugins.yaml \
+  https://raw.githubusercontent.com/aaron-dm-mcdonald/new-jenkins-s3-test/refs/heads/main/plugins.yaml
 
-java -jar /home/ec2-user/jenkins-plugin-manager-2.14.0.jar \
+sudo -u jenkins java -jar /tmp/jenkins-plugin-manager.jar \
   --war /usr/share/java/jenkins.war \
   --plugin-download-directory /var/lib/jenkins/plugins \
-  --plugin-file plugins.yaml
+  --plugin-file /tmp/plugins.yaml
 
 # restart service
 systemctl daemon-reload
