7d726811d6d121fcf6b2050acd008c0bc10f2909
Kevin Tilghman <55026237+tunerzk@users.noreply.github.com>
2026-03-25 16:29:05 -0400

    Upgrade AWS provider version to 6.0

--- test-bucket.tf
+++ test-bucket.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # Use latest version if possible
+      version = "~> 6.0" # Use latest version if possible
     }
   }
 
