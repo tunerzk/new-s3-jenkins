ef561f38f5ec0e3d8285fab5302169b0c82fec97
Kevin Tilghman <55026237+tunerzk@users.noreply.github.com>
2026-04-14 20:53:33 -0400

    Update terraforms3.tf

--- terraforms3.tf
+++ terraforms3.tf
@@ -16,14 +16,14 @@ locals {
 resource "aws_s3_object" "uploads" {
   for_each = { for f in local.upload_files : f => f }
 
-  bucket = "theo-jenkins-s3-test20260330194114774900000001 "
+  bucket = "theo-jenkins-s3-test20260330194114774900000001"
   key    = "uploads/${each.value}"
   source = "uploads/${each.value}"
   etag   = filemd5("uploads/${each.value}")
 }
 
 resource "aws_s3_object" "upload_image" {
-  bucket = "theo-jenkins-s3-test20260330194114774900000001 "
+  bucket = "theo-jenkins-s3-test20260330194114774900000001"
   key    = "uploads/8978169.jpg"
   source = "uploads/8978169.jpg"
   etag   = filemd5("uploads/8978169.jpg")
